ee5c0920145f9f9668cec0e9babde3359f74e361
Nikola Kuzmic <nikola.kuzmic945@gmail.com>
2022-10-08 12:42:41 -0400

    Issue when reading parquet file using spark-submit - likely due to InaccessibleObjectException

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -16,10 +16,12 @@ object HelloWorld {
 
 //    df.write.parquet("./src/main/resources/data")
 
-    println("Reading parquet file")
-
-    val parqDF = spark.read.parquet("src/main/resources/data/part-00000-aa642a6d-fc9d-4c7d-82cd-dff5b8afa1d3-c000.snappy.parquet")
-    parqDF.show()
+//    println("Reading parquet file")
+//
+//    val parqDF = spark.read.parquet("src/main/resources/data/part-00000-aa642a6d-fc9d-4c7d-82cd-dff5b8afa1d3-c000.snappy.parquet")
+////    val parqDF = spark.read.parquet("/Users/nikolakuzmic/Desktop/scala-spark-hw/src/main/resources/data/part-00000-aa642a6d-fc9d-4c7d-82cd-dff5b8afa1d3-c000.snappy.parquet")
+//
+//    parqDF.show()
   }
 
 }
