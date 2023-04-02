299021bc8ff59fb0909e402c739d78a7385b6b0e
Nikola Kuzmic <nikola.kuzmic945@gmail.com>
2023-04-02 16:46:08 -0400

    added a udf and observing spark app performance with different configs

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,5 +1,5 @@
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.{DataFrame, SQLContext}
+import org.apache.spark.sql.functions.{col, udf, when}
+import org.apache.spark.sql.{DataFrame, SQLContext, SparkSession, functions}
 
 object HelloWorld {
   def main(args: Array[String]): Unit = {
@@ -13,6 +13,26 @@ object HelloWorld {
     val df = spark.createDataFrame(sampSeq).toDF(columns:_*)
 //    val newDataDF = sqlContext.read.parquet("Sales.parquet")
     df.show()
+
+    // Generate 1000 random words
+    val words = Seq.fill(1000000)(scala.util.Random.alphanumeric.take(10).mkString)
+
+    // Create DataFrame with words and row numbers
+    val dfWords = spark.createDataFrame(words.zipWithIndex)
+      .toDF("word", "row_num")
+
+    var a = dfWords.count();
+
+    println(s"----------- Num of ROWS: $a ----------------")
+
+    val sumCol = dfWords.agg(functions.sum("row_num")).collect()(0)(0)
+    println(s"----------- SUM of ROW NUMBERS: $sumCol ----------------")
+
+    val containsTwoAs = udf((s: String) => s.count(_ == 'U') == 1)
+    val dfWY = dfWords.withColumn("y", when(containsTwoAs(col("word")), 1).otherwise(0))
+    dfWY.show()
+    val sumColU = dfWY.agg(functions.sum("y")).collect()(0)(0)
+    println(s"----------- SUM of U WORDS: $sumColU ----------------")
 
 //    df.write.mode("overwrite").parquet("./src/main/resources/data")
 
