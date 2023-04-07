afd319e15b77740cb4ab78cda623eb857420a3d5
Nikola Kuzmic <nikola.kuzmic945@gmail.com>
2023-04-07 13:12:08 -0400

    set 6 worker threads in local[6], store spark-ui files in spark-events folder, and pass an arg to scala app from spark-submit

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,11 +1,27 @@
-import org.apache.spark.sql.functions.{col, udf, when}
-import org.apache.spark.sql.{DataFrame, SQLContext, SparkSession, functions}
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.functions.{col, udf, when, sum}
+import org.apache.spark.sql.{DataFrame, SQLContext, SparkSession}
 
 object HelloWorld {
   def main(args: Array[String]): Unit = {
     println("HelloWorld")
 
-    val spark = SparkSession.builder().appName("HelloSpark").config("spark.master", "local").config("spark.driver.host","127.0.0.1").getOrCreate()
+//    val myArg = args(0)
+//    println(s"my ARG: $myArg")
+
+    val sparkConf = new SparkConf()
+      .setAppName("MySparky")
+      .setMaster("local[6]")
+      .set("spark.eventLog.enabled", "true")
+      .set("spark.eventLog.dir","/Users/nikolakuzmic/spark-events")
+
+//    val sparkContext = new SparkContext(sparkConf)
+
+    //    val spark = SparkSession.builder().appName("HelloSpark").config("spark.master", "local").config("spark.driver.host","127.0.0.1").config("spark.eventLog.dir","/Users/nikolakuzmic/spark-events").getOrCreate()
+    val spark = SparkSession.builder().config(sparkConf).getOrCreate()
+
+    val eventLogDir = sparkConf.get("spark.eventLog.dir")
+    println(s"Event log directory: $eventLogDir")
 
     val sampSeq =Seq((1,"spark"), (2,"hadoop"))
     val columns = Seq("num","tech")
@@ -15,7 +31,7 @@ object HelloWorld {
     df.show()
 
     // Generate 1000 random words
-    val words = Seq.fill(1000000)(scala.util.Random.alphanumeric.take(10).mkString)
+    val words = Seq.fill(300000)(scala.util.Random.alphanumeric.take(10).mkString)
 
     // Create DataFrame with words and row numbers
     val dfWords = spark.createDataFrame(words.zipWithIndex)
@@ -25,13 +41,13 @@ object HelloWorld {
 
     println(s"----------- Num of ROWS: $a ----------------")
 
-    val sumCol = dfWords.agg(functions.sum("row_num")).collect()(0)(0)
+    val sumCol = dfWords.agg(sum("row_num")).collect()(0)(0)
     println(s"----------- SUM of ROW NUMBERS: $sumCol ----------------")
 
     val containsTwoAs = udf((s: String) => s.count(_ == 'U') == 1)
     val dfWY = dfWords.withColumn("y", when(containsTwoAs(col("word")), 1).otherwise(0))
     dfWY.show()
-    val sumColU = dfWY.agg(functions.sum("y")).collect()(0)(0)
+    val sumColU = dfWY.agg(sum("y")).collect()(0)(0)
     println(s"----------- SUM of U WORDS: $sumColU ----------------")
 
 //    df.write.mode("overwrite").parquet("./src/main/resources/data")
